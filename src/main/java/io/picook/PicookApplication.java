552b7713c04e3618b403c2579b268a0d914ff37c
민수 <alstn7223@gmail.com>
2026-02-09 16:39:17 +0900

    [20260209] 프로젝트 초기 세팅
    도메인/글로벌/인프라 디렉토리 구조로 나누어 초기 세팅 및 환경변수 파일 초기값 설정

---
+++ src/main/java/io/picook/PicookApplication.java
@@ -0,0 +1,13 @@
+package io.picook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicookApplication.class, args);
+	}
+
+}
